46c61bdd352606fd9da3936c5f6ddf522c164aa9
Eduardo Ruelas Cortés <eduruelas13@gmail.com>
2023-02-26 02:31:30 -0600

    Sidebar

---
+++ pages/about.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import time
+import numpy as np
+
+st.set_page_config(page_title="Acerca de nostros", page_icon="📈")
+st.sidebar.header('Barra de navegación')
+st.title('Acerca de nostros')
+
+st.header('¿Quienes somos?')
+
+st.header('Objetivos')
+
+st.header('Metas')
+
+st.header('Algo más...')
